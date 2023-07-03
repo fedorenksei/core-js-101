5e362e86bcb35a0dd92cd1abdde31144284317ff
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-07-04 00:18:03 +0300

    feat: solve date tasks but one

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,9 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  // throw new Error('Not implemented');
+  return Date.parse(value);
 }
 
 
@@ -53,8 +54,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const Feb29 = new Date(date.getFullYear(), 1, 29);
+  return Feb29.getDate() === 29;
 }
 
 
@@ -73,8 +75,28 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  // throw new Error('Not implemented');
+  if (!(startDate instanceof Date) || !(endDate instanceof Date)) return null;
+
+  let hours = endDate.getHours() - startDate.getHours();
+  let minutes = endDate.getMinutes() - startDate.getMinutes();
+  if (minutes < 0) {
+    hours -= 1;
+    minutes = 60 - minutes;
+  }
+  let seconds = endDate.getSeconds() - startDate.getSeconds();
+  if (seconds < 0) {
+    minutes -= 1;
+    seconds = 60 - seconds;
+  }
+  let milliSeconds = endDate.getMilliseconds() - startDate.getMilliseconds();
+  if (milliSeconds < 0) {
+    minutes -= 1;
+    milliSeconds = 1000 - milliSeconds;
+  }
+
+  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(milliSeconds).padStart(3, '0')}`;
 }
 
 
